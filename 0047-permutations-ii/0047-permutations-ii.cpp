3147e04543b3eb2d1cfd9b87b302c03ae4d2cba6
hrithik19092006 <hrithik28289@gmail.com>
2026-07-02 15:12:06 +0530

    Time: 3 ms (62.97%), Space: 11.5 MB (72.03%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,19 +1,29 @@
 class Solution {
-    void generate(int index , vector<int>&nums , set<vector<int>>&ans){
+    void generate(int index , vector<vector<int>>&ans ,vector<int>&nums , vector<int>&freq , vector<int>&temp){
         if(index == nums.size()){
-            ans.insert(nums);
+            ans.push_back(temp);
             return;
         }
-        for(int i = index ; i < nums.size() ; i++){
-            swap(nums[index] , nums[i]);
-            generate(index + 1 , nums , ans);
-            swap(nums[index] , nums[i]);
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(freq[i] != 1){
+                if(i > 0 && nums[i] == nums[i-1] && freq[i-1] == 0){
+                    continue;
+                }
+                temp.push_back(nums[i]);
+                freq[i] = 1;
+                generate(index + 1 , ans ,nums , freq , temp );
+                freq[i] = 0;
+                temp.pop_back();
+            }
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>>ans;
-        generate(0 , nums , ans);
-        return vector<vector<int>>(ans.begin() , ans.end());
+        sort(nums.begin() , nums.end());
+        vector<vector<int>>ans;
+        vector<int>freq(nums.size() , 0);
+        vector<int>temp;
+        generate(0 , ans , nums , freq, temp);
+        return ans;
     }
 };
