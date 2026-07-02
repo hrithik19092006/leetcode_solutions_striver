494f9e00d2ebc7962746e6d18928118886712780
hrithik19092006 <hrithik28289@gmail.com>
2026-07-02 15:09:32 +0530

    Time: 0 ms (100%), Space: 10.7 MB (50.6%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,25 @@
 class Solution {
-    void generate(int index , vector<int>&nums , vector<vector<int>>&ans){
+    void generate(int index , vector<vector<int>>&ans ,vector<int>&nums , vector<int>&freq , vector<int>&temp){
         if(index == nums.size()){
-            ans.push_back(nums);
+            ans.push_back(temp);
             return;
         }
-        for(int i = index ; i < nums.size() ; i++){
-            swap(nums[index] , nums[i]);
-            generate(index + 1 , nums , ans);
-            swap(nums[index] , nums[i]);
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(freq[i] != 1){
+                temp.push_back(nums[i]);
+                freq[i] = 1;
+                generate(index + 1 , ans ,nums , freq , temp );
+                freq[i] = 0;
+                temp.pop_back();
+            }
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        generate(0 , nums , ans);
+        vector<int>freq(nums.size() , 0);
+        vector<int>temp;
+        generate(0 , ans , nums , freq, temp);
         return ans;
     }
 };
