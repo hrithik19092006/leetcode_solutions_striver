1c0d7e22a57f90b8d23574d6d8baf5307048c9a5
hrithik19092006 <hrithik28289@gmail.com>
2026-07-11 13:45:38 +0530

    Time: 0 ms (100%), Space: 21.4 MB (96.63%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int n = board.size();
-        for(int i = 0 ; i < n ;i++){
-            for(int j = 0 ; j < n ;j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
                 if(board[i][j] != '.'){
                     for(int k = 0 ; k < n ; k++){
                         if(k != j && board[i][k] == board[i][j]){
@@ -12,8 +12,8 @@ public:
                         if(k != i && board[k][j] == board[i][j]){
                             return false;
                         }
-                        int x = 3 * (i /3) + k/3;
-                        int y = 3 * (j/3) + k % 3 ;
+                        int x = 3 * (i/3) + k /3;
+                        int y = 3 * (j/3) + k % 3;
                         if((x != i || y != j) && board[x][y] == board[i][j]){
                             return false;
                         }
