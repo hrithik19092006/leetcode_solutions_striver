6599ba5bd99beee605979e03b102a57a8c326a55
hrithik19092006 <hrithik28289@gmail.com>
2026-08-06 01:09:06 +0530

    Time: 3 ms (52.03%), Space: 11.8 MB (49.67%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char , int> mpp;
+        for(auto it : magazine){
+            mpp[it] ++;
+        }
+        for(auto it : ransomNote){
+            if(mpp[it]> 0){
+                mpp[it]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
