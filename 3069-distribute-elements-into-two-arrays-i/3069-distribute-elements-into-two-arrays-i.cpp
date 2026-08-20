536519948078844af5588b2856f85155f6951a76
hrithik19092006 <hrithik28289@gmail.com>
2026-08-20 21:46:46 +0530

    Time: 4 ms (12.56%), Space: 23.9 MB (66.37%) - LeetHub

---
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int>ans1;
+        vector<int>ans2;
+        ans1.push_back(nums[0]);
+        ans2.push_back(nums[1]);
+        for(int i = 2 ; i <nums.size() ; i++){
+            if(ans1.back() > ans2.back()){
+                ans1.push_back(nums[i]);
+            }
+            else{
+                ans2.push_back(nums[i]);
+            }
+        }
+        ans1.insert(ans1.end() , ans2.begin() , ans2.end());
+        return ans1;
+    }
+};
