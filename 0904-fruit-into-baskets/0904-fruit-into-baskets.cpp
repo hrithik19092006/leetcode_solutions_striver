de1c5619dd8f869a5e8d28a6a214739bbc757586
hrithik19092006 <hrithik28289@gmail.com>
2026-08-23 23:59:00 +0530

    Time: 41 ms (65.45%), Space: 84.8 MB (63.2%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -6,14 +6,16 @@ public:
         int left =0 ;
         for(int right = 0 ; right< nums.size() ; right++){
             mpp[nums[right]]++;
-            while(mpp.size() > 2){
+            if(mpp.size() > 2){
                 mpp[nums[left]] --;
                 if(mpp[nums[left]] == 0){
                     mpp.erase(nums[left]);
                 }
                 left++;
             }
-            maxlen = max(maxlen , right - left + 1 );
+            if(mpp.size() <= 2){
+                maxlen = max(maxlen , right - left + 1 );
+            }
         }
         return maxlen;
     }
