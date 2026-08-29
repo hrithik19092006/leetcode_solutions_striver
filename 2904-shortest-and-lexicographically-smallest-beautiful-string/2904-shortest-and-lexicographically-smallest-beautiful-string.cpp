f8dd60f831bd9f5182180cecbed464618341874a
hrithik19092006 <hrithik28289@gmail.com>
2026-08-29 13:52:29 +0530

    Time: 11 ms (5.7%), Space: 10.6 MB (9.85%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         string ans = "";
-        int left = 0 ;
-        int count = 0;
-        for(int right = 0 ; right < s.size() ; right ++){
-            if(s[right] == '1'){
-                count ++;
-            }
-            while(left<=right && (count > k||count ==k &&s[left] == '0')){
-                if(s[left] == '1'){
-                    count --;
+        for(int i = 0 ; i < s.size() ; i++){
+            string temp = "";
+            int count = 0 ;
+            for(int j = i ; j < s.size() ; j++){
+                temp.push_back(s[j]);
+                if(s[j] == '1'){
+                    count ++;
                 }
-                left ++;
-            }
-            if(count == k){
-                string temp = s.substr(left , right -left + 1);
-                int a = ans.size();
-                int b = temp.size();
-                if(ans == "" || a > b ||( a == b && ans > temp)){
-                    ans = temp;
+                if(count == k){
+                    temp = s.substr(i , j - i + 1);
+                    int a = ans.size();
+                    int b = temp.size();
+                    if(ans == "" || a > b || (a == b && ans > temp)){
+                        ans = temp ;
+                    }
                 }
             }
         }
