ea828ca7c79c0a5feadfab14665081d44e6e366f
hrithik19092006 <hrithik28289@gmail.com>
2026-09-13 09:35:28 +0530

    Time: 27 ms (10%), Space: 35.1 MB (20%) - LeetHub

--- 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
-        unordered_map<int , int>mpp;
-        for(auto it : nums){
-            mpp[it] ++;
-        }
-        vector<int > temp ;
-        for(auto it : mpp){
-            if(it.second == 3){
-                temp.push_back(it.first);
-            }
-        }
-        if(temp.size() == 0)return 0;
-        unordered_map<int, int> freq;
-        for(auto it : temp){
-            freq[it]++;
+        unordered_map<int , vector<int>>mpp;
+        for(int i = 0 ; i < nums.size() ; i++){
+            mpp[nums[i]].push_back(i);
         }
         int count = 0 ;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(freq.find(nums[i]) != freq.end()){
-                int temp = nums[i];
-                int first = i;
-                int second = -1;
-                int third = -1;
-                for(int j = i+1; j < nums.size() ; j++){
-                    if(nums[j] == temp){
-                        if(second == -1)second= j;
-                        else{
-                            third = j;
-                        }
-                    }
-                    if(third != -1)break;
-                }
-                if(third != -1 && second != -1){
-                    if(third - second == second - first)count++;
+        for(auto it : mpp){
+            vector<int>indices = it.second;
+            if(indices.size() == 3){
+                if(indices[2] - indices[1] == indices[1] - indices[0]){
+                    count ++;
                 }
             }
         }
