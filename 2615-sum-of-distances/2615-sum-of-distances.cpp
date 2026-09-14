ba0b31448fa212d72f40ce9351227f94fcd27131
hrithik19092006 <hrithik28289@gmail.com>
2026-09-14 13:48:01 +0530

    Time: 117 ms (14.37%), Space: 136.4 MB (21.92%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    typedef long long ll;
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int , ll> freq;
+        unordered_map<int , ll> indexsum;
+        vector<ll> ans(n , 0);
+
+        //left to right
+        for(int i = 0 ;i < n ; i++){
+            ll count = freq[nums[i]];
+            ll sum = indexsum[nums[i]];
+
+            ans[i] += count * i  - sum; 
+
+            freq[nums[i]]++;
+            indexsum[nums[i]] += i;
+        }
+
+        freq.clear();
+        indexsum.clear();
+
+        //right to left
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            ll count = freq[nums[i]];
+            ll sum = indexsum[nums[i]];
+
+            ans[i]+= sum - count * i;
+
+            freq[nums[i]]++;
+            indexsum[nums[i]] += i;
+        }
+        return ans;
+    }
+};
