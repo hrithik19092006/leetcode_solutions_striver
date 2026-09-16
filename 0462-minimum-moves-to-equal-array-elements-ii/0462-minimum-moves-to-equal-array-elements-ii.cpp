6189581aae6f571e2a0440402a67679de3ac7fe0
hrithik19092006 <hrithik28289@gmail.com>
2026-09-17 01:03:26 +0530

    Time: 3 ms (29.3%), Space: 16 MB (2.58%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,20 +3,11 @@ public:
     int minMoves2(vector<int>& nums) {
         sort(nums.begin() , nums.end());
         int n = nums.size();
-        int x = nums[n/2];
-        vector<int>prefix(n);
-        prefix[0] = nums[0];
-        for(int i = 1 ; i < n ; i++){
-            prefix[i] = prefix[i-1] + nums[i];
+        int median = nums[n/2];
+        int count = 0 ;
+        for(auto it : nums){
+            count += abs(it - median);
         }
-        int lower = lower_bound(nums.begin() , nums.end() , x) - nums.begin();
-        
-        int left_sum = (lower > 0 )? prefix[lower-1] : 0;
-        int left_cost = (x * lower) - left_sum;
-
-        int right_sum = prefix[n-1] - left_sum;
-        int right_cost = right_sum - (x * (n - lower));
-
-        return right_cost + left_cost;
+        return count;
     }
 };
